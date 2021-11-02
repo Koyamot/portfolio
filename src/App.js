9cbc0c06477684ab12d3ac9ba49c8436ad02448d
Kyla Oyamot <koyamot@gmail.com>
2021-11-02 12:55:23 -0700

    start breakpoints

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 // import { useEffect, useState } from 'react';
-import './App.scss';
 
-import Header from "./components/Header"
+import Header from "./components/common/Header"
 
 function App() {
 
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,13 @@
 import React from "react";
-import "./Header.scss";
 
 
 
 const Header = () => {
     return (
         <header>
-        <h2>The Portfolio of Kyla Oyamot</h2>
+        <h2>i am kyl</h2>
         <nav>
-          <a className="header-a" href="#">Porfolio</a>
+          <a className="header-a" href="#">Home</a>
           <a className="header-a" href="#">About Me</a>
           <a className="header-a" href="#">Contact</a>
         </nav>
