71f0cc0a3e65cb016d9ba0b66c2c1d78aaf6f78b
Kyla Oyamot <koyamot@gmail.com>
2021-11-02 13:40:36 -0700

    typo fix

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-        <h2>i am kyl</h2>
+        <h2>hello. i am kyla.</h2>
         <nav>
           <a className="header-a" href="#">Home</a>
           <a className="header-a" href="#">About Me</a>
