567ddfe623960c7cf6ef1e31b969597e268fbd5a
Kyla Oyamot <koyamot@gmail.com>
2021-11-02 19:57:15 -0700

    added graphql/apollo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { createHttpLink, ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 // import { useEffect, useState } from 'react';
 
 import Header from "./components/common/Header"
 
-function App() {
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
+function App() {
+  const link = createHttpLink({
+    uri: `${backendUrl}/graphql`,
+    credentials: 'same-origin'
+  })
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link
+  });
   return (
+    <ApolloProvider client={client}>
     <main>
       <Header />
     </main>
+    </ApolloProvider>
   );
 }
 
