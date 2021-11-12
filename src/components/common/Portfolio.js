5af85ab9ef1b62579aa411c824d4c5923de3111d
Kyla Oyamot <koyamot@gmail.com>
2021-11-12 13:18:12 -0800

    change title

--- src/components/common/Portfolio.js
+++ src/components/common/Portfolio.js
@@ -29,10 +29,6 @@ const Portfolio = () => {
       <h1 className="portfolio_h1">
         View my gallery of visual human experiences
       </h1>
-      <h4>
-        Hi! Thanks for looking. Because I am a UX Engineer, I wanted to display
-        my UX skills and engineering skills in the same{" "}
-      </h4>
       <div className="hold_portfolio">
         {portfolios.map((x, i) => (
           <Link to={`/portfolio/${x.id}`} key={i}>
